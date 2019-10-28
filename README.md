7b5fb4ca4b5d1ea5dab993f852e83d3ebef209f9
Julian <rustycontraption@gmail.com>
2019-10-28 15:59:47 -0700

    Refactoring

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# web
+In root folder of project:
+
+Create virtual env:
+``` virtualenv .env
+source .env/bin/activate ```
+
+#requirements
+flask
+zappa
+
+```pip3 install -r requirements```
+
+From wherever flask app.py is:
+```zappa init```
---
+++ app.py
@@ -0,0 +1,53 @@
+import json
+import requests
+
+from urllib.parse import urlparse
+from flask import Flask, Response, render_template, redirect, session, request, g, url_for, jsonify
+
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+     return render_template(
+            "index.html",
+            heading="projects",
+            subheading="build, break, fix, repeat",
+            next="",
+            prev=""
+        )
+@app.route('/mast')
+def mast():
+     return render_template(
+            "mast.html",
+            heading="mast",
+            subheading="testing stuff",
+            next="rabbit",
+            prev="gs750"
+        )
+     
+@app.route('/rabbit')
+def rabbit():
+     return render_template(
+            "rabbit.html",
+            heading="rabbit",
+            subheading="testing stuff",
+            next="gs750",
+            prev="mast"
+        )
+
+@app.route('/gs750')
+def gs750():
+     return render_template(
+            "gs750.html",
+            heading="gs750",
+            subheading="testing stuff",
+            next="mast",
+            prev="rabbit"
+        )
+
+
+
+if __name__ == '__main__':
+     app.run()
