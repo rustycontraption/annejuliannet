488ddb1bb8f55a90eb03db65af0385aa3e1657ab
julian <rustycontraption@gmail.com>
2019-10-28 19:45:42 -0700

    Fixing drop down nav

--- app.py
+++ app.py
@@ -47,7 +47,17 @@ def gs750():
             prev="rabbit"
         )
 
+@app.route('/about')
+def about():
+     return render_template(
+           "about.html"
+     )
 
+@app.route('/contact')
+def contact():
+     return render_template(
+           "contact.html"
+     )
 
 if __name__ == '__main__':
      app.run()
